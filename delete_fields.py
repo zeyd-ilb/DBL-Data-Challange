671c7f9517ceda21169a146ce3a4782af96d2345
zeyd-ilb <61659041+zeyd-ilb@users.noreply.github.com>
2024-05-15 00:17:34 +0200

    Update delete_fields.py
    data tranfer from extended_tweet to original tweet is added.
    extended_tweet.full_text --> text
    extended_tweet.display_text_range --> display_text_range (if exist)
    extended_tweet.entities --> entities

--- delete_fields.py
+++ delete_fields.py
@@ -3,41 +3,80 @@ import json
 import time
 
 # start = time.time()
+a = {
+    "full_text": None,
+    "display_text_range": None,
+    "entities": None
+}
+def check_null(full_key,data,key):
+    skipp = None
+    if data[key] is None:
+        del data[key]
+        skipp = True
+    return data,key, skipp
 
-def check_retweets(full_key,data,key,rt):
+def check_retweets(full_key, data, key, rt, gbd):
     if full_key == "retweeted_status":
         rt = True
     elif rt and full_key == "is_quote_status":
         if data[key] == False:
             gbd = True
-            # print('data will be deleted')
     return data,key, rt,gbd
 
-def check_truncated(full_key,data,key):
+def check_truncated(full_key,data,key,tr):
     if full_key == "truncated" and data[key] == True:
-        del data['text']
-    return data,key
+        tr = True
+        # if 'text' in data:
+        #     del data['text']
+    return data,key, tr
 
-def delete_fields(data, fields_to_delete, rt, parent_key='' ):
-    gbd = None 
+def record_text(data,key,pull_push):
+    global a
+    if pull_push:
+        a['full_text'] = data['full_text']
+        a['entities'] = data['entities']
+        a['display_text_range'] = data['display_text_range']
+    else:
+        return a
+
+def tranfer_from_extended_to_original(original):
+    transfers = record_text(data='',key='',pull_push=False)
+    original['text'] = transfers['full_text']
+    for k in transfers.keys():
+        if k != "full_text" and k in original:
+            original[k] = transfers[k]
+    return original
+
+def delete_fields(data, fields_to_delete, rt, trunc, parent_key='' ):
+    gbd = None
+    skip = None
     if isinstance(data, dict):
         for key in list(data.keys()):
             # Build the full key path including parent keys if present
             full_key = f"{parent_key}.{key}" if parent_key else key
-            
-            data,key,rt, gbd = check_retweets(full_key,data,key,rt)
-            data,key = check_truncated(full_key,data,key)
 
+            data,key,rt, gbd = check_retweets(full_key, data, key, rt, gbd)
+            data,key, trunc = check_truncated(full_key,data,key,trunc)
+            data,key,skip = check_null(full_key, data,key)
+            
             if full_key in fields_to_delete:
+                print("this one has been deleted" , key)
+
                 del data[key]
+            elif trunc and (full_key in fields_to_transfer):
+                record_text(data,key,True)
+
             else:
-                # Recursively call delete_fields with the updated key path
-                delete_fields(data[key], fields_to_delete,rt, full_key)
+                if not skip:
+                    # Recursively call delete_fields with the updated key path
+                    delete_fields(data[key], fields_to_delete,rt, trunc, full_key)
+
     elif isinstance(data, list):
         for item in data:
             # Recursively call delete_fields for each item in the list
-            delete_fields(item, fields_to_delete, rt, parent_key)
+            delete_fields(item, fields_to_delete, rt,trunc, parent_key)
     return gbd
+
 
 def process_json_files(folder_path, fields_to_delete):
     # Get list of JSON files in the folder
@@ -51,8 +90,11 @@ def process_json_files(folder_path, fields_to_delete):
             # Read each line from the file and process JSON data
             for line in f:
                 retweeted = False
+                truncated = False
                 original_data = json.loads(line)
-                gon_be_deleted = delete_fields(original_data, fields_to_delete,retweeted)
+                gon_be_deleted = delete_fields(original_data, fields_to_delete,retweeted, truncated)
+                original_data = tranfer_from_extended_to_original(original_data)
+
                 if gon_be_deleted:
                     deleted_lines.append(json.dumps(original_data) + '\n')
                 else: 
@@ -62,6 +104,7 @@ def process_json_files(folder_path, fields_to_delete):
 
 if __name__ == "__main__":
     folder_path = "C:\\Users\\20223070\\Downloads\\deneme data\\yan"  # Path to the folder containing JSON files
+    fields_to_transfer = ["extended_tweet.full_text"]
     fields_to_delete = ["id_str"]  # List of fields to delete
     process_json_files(folder_path, fields_to_delete)
     # end = time.time()
