b201e0a6a12674502ad7a452a8da07d92a01c81f
zeyd-ilb <61659041+zeyd-ilb@users.noreply.github.com>
2024-05-14 03:15:48 +0200

    Updated delete_fields.py
    Strcuture for removol of RT's is formed

--- delete_fields3.py
+++ delete_fields3.py
@@ -1,19 +1,22 @@
 import os
 import json
+import time
 
-def delete_fields(data, fields_to_delete, parent_key=''):
+# start = time.time()
+
+def delete_fields(data, fields_to_delete, rt, parent_key='' ):
     if isinstance(data, dict):
         for key in list(data.keys()):
             # Build the full key path including parent keys if present
             full_key = f"{parent_key}.{key}" if parent_key else key
-
+            
             #Check if it is retweet
             if full_key == "retweeted_status":
                 rt = True
             elif rt and full_key == "is_quote_status":
                 if data[key] == False:
                     print('data will be deleted')
-            
+                    
             #Casual check for fields to delete
             if full_key in fields_to_delete:
                 del data[key]
@@ -23,7 +26,7 @@ def delete_fields(data, fields_to_delete, parent_key=''):
     elif isinstance(data, list):
         for item in data:
             # Recursively call delete_fields for each item in the list
-            delete_fields(item, fields_to_delete, parent_key)
+            delete_fields(item, fields_to_delete, rt, parent_key)
 
 def process_json_files(folder_path, fields_to_delete):
     # Get list of JSON files in the folder
@@ -35,17 +38,18 @@ def process_json_files(folder_path, fields_to_delete):
         with open(file_path,"r+") as f:
             # Read each line from the file and process JSON data
             for line in f:
+                retweeted = False
                 original_data = json.loads(line)
-                delete_fields(original_data, fields_to_delete)
-                print(original_data)
-                print('---------------------------------------------------------------------------------------')
+                delete_fields(original_data, fields_to_delete,retweeted)
                 modified_lines.append(json.dumps(original_data) + '\n')
+        
         with open(file_path,'w+') as f:        
             f.writelines(modified_lines)
 
 
 if __name__ == "__main__":
-    
     folder_path = "C:\\Users\\20223070\\Downloads\\deneme data\\yan"  # Path to the folder containing JSON files
-    fields_to_delete = ["id_str","entities.hashtags","entities.urls"]  # List of fields to delete
+    fields_to_delete = ["id_str"]  # List of fields to delete
     process_json_files(folder_path, fields_to_delete)
+    # end = time.time()
+    # print(end - start)
