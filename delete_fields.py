eb593df2a5a163c2b979005e36f8e9605c61c6d9
zeyd-ilb <61659041+zeyd-ilb@users.noreply.github.com>
2024-05-15 04:57:01 +0200

    Update delete_fields.py
    mini final version

--- delete_fields.py
+++ delete_fields.py
@@ -1,6 +1,10 @@
 import os
 import json
 import time
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='app.log', filemode='w')
 
 start = time.time()
 a = {
@@ -8,26 +12,27 @@ a = {
     "display_text_range": None,
     "entities": None
 }
+line_number = None
+file_name = None
+
 def check_null(full_key,data,key):
     skipp = None
     if data[key] is None:
         del data[key]
         skipp = True
+        #logging.info(f"Deleted key {full_key} because it was null")
     return data,key, skipp
 
-def check_retweets(full_key, data, key, rt, gbd):
+def check_retweets(full_key, gbd):
     if full_key == "retweeted_status":
-        rt = True
-    elif rt and full_key == "is_quote_status":
-        if data[key] == False:
-            gbd = True
-    return data,key, rt,gbd
+        gbd = True
+        #logging.info(f"Found retweet status at line {line_number}")
+    return gbd
 
 def check_truncated(full_key,data,key,tr):
     if full_key == "truncated" and data[key] == True:
         tr = True
-        # if 'text' in data:
-        #     del data['text']
+        #logging.info(f"Found truncated status at key {full_key}")
     return data,key, tr
 
 def record_text(data,key,pull_push):
@@ -35,6 +40,10 @@ def record_text(data,key,pull_push):
     if pull_push:
         a['full_text'] = data['full_text']
         a['entities'] = data['entities']
+        if "extended_entities" in data:
+            a['extended_entities'] = data['extended_entities']
+            logging.info(f"Recorded text and entities for key extended_entities in line {line_number}")
+        #logging.info(f"Recorded text and entities for key {key} in line {line_number}")
     else:
         return a
 
@@ -47,6 +56,8 @@ def tranfer_from_extended_to_original(original):
             original[k] = transfers[k]
     if "extended_tweet" in original:
         del original['extended_tweet']
+        #logging.info(f"Transferred data from extended_tweet to original")
+
     return original
 
 def delete_fields(data, fields_to_delete,keys_to_delete, rt, trunc, parent_key='' ):
@@ -57,17 +68,20 @@ def delete_fields(data, fields_to_delete,keys_to_delete, rt, trunc, parent_key='
             # Build the full key path including parent keys if present
             full_key = f"{parent_key}.{key}" if parent_key else key
 
-            data,key,rt, gbd = check_retweets(full_key, data, key, rt, gbd)
+            gbd = check_retweets(full_key, gbd)
             if gbd:
+                logging.info(f"The line number {line_number} will be deleted from file {file_name} ")
+
                 return gbd , trunc
             data,key, trunc = check_truncated(full_key,data,key,trunc)
             data,key,skip = check_null(full_key, data,key)
 
             if not skip:
                 if (full_key in fields_to_delete or key in keys_to_delete):
-                    print("this one has been deleted" , full_key)
-
+                    #print("this one has been deleted" , full_key)
                     del data[key]
+                    #logging.info(f"Deleted key {full_key}")
+
                 elif trunc and (full_key in fields_to_transfer):
                     record_text(data,key,True)
 
@@ -88,27 +102,36 @@ def process_json_files(folder_path, fields_to_delete):
     for json_file in json_files:
         file_path = os.path.join(folder_path, json_file)
         modified_lines = []
-        deleted_lines = []
 
+        global file_name
+        file_name = json_file
+
+        logging.info(f"Processing file: {json_file}")
         with open(file_path,"r+") as f:
+            global line_number
+            line_number = 0
             # Read each line from the file and process JSON data
             for line in f:
-                retweeted = False
-                truncated = False
-                original_data = json.loads(line)
-                gon_be_deleted, truncated = delete_fields(original_data, fields_to_delete,keys_to_delete,retweeted, truncated)
+                line_number += 1
+                if "Exceeded connection limit for user" not in line:
+                    retweeted = False
+                    truncated = False
+                    try:
+                        original_data = json.loads(line)
+                        gon_be_deleted, truncated = delete_fields(original_data, fields_to_delete,keys_to_delete,retweeted, truncated)
 
-                if truncated and not gon_be_deleted:
-                    original_data = tranfer_from_extended_to_original(original_data)
-                if gon_be_deleted:
-                    deleted_lines.append(json.dumps(original_data) + '\n')
-                else: 
-                    modified_lines.append(json.dumps(original_data) + '\n')
+                        if truncated and not gon_be_deleted:
+                            original_data = tranfer_from_extended_to_original(original_data)
+                        if not gon_be_deleted: 
+                            modified_lines.append(json.dumps(original_data) + '\n')
+                    except:
+                        logging.error(f"ERROR occured at file: {json_file} in line {line_number}")
+                               
         with open(file_path,'w+') as f:        
            f.writelines(modified_lines)
 
 if __name__ == "__main__":
-    folder_path = "C:\\Users\\20223070\\Downloads\\deneme data\\yan"  # Path to the folder containing JSON files
+    folder_path = "C:\\Users\\20223070\\Downloads\\deneme data\\orta"  # Path to the folder containing JSON files
     fields_to_transfer = ["extended_tweet.full_text"]
     fields_to_delete = ["created_at"]  # List of fields to delete
     keys_to_delete = ["indices","display_text_range","media_url","media_url_https","display_url","expanded_url","id_str","in_reply_to_status_id_str",
@@ -116,4 +139,5 @@ if __name__ == "__main__":
                       "profile_image_url"]
     process_json_files(folder_path, fields_to_delete)
     end = time.time()
+    logging.info(f"Processing completed in {end - start} seconds")
     print(end - start)
