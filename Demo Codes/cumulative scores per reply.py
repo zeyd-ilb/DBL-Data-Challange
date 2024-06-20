94f68d60cf0a511225ef7ceb72f5636a8b6dd8ec
zeyd-ilb <61659041+zeyd-ilb@users.noreply.github.com>
2024-06-20 03:06:28 +0200

    Update cumulative scores per reply.py

--- Demo Codes/cumulative scores per reply.py
+++ Demo Codes/cumulative scores per reply.py
@@ -10,7 +10,7 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['Chains']
 collection = db['chains_British_Airways_v3']
 compound_scores_collection = db['compound_scores_3m']
-
+    
 pipeline = [
     {
         '$project': {
@@ -37,6 +37,24 @@ pipeline = [
             }
         }
     }, 
+    # {
+    #     '$match': {
+    #         'chains': {
+    #             '$elemMatch': {
+    #                 '$elemMatch': {
+    #                     'timestamp_ms': 
+    #                       '$expr': {
+                    #             "$and":[
+                    #             {'$gt': [{'$size': '$chains'}, 0]},
+                    #             {"$lt": [{ "$size": "$chains" }, 10]}
+                    #             ]
+    #                       }
+    #                 }
+    #             }
+    #         }
+    #     }
+    # },
+    
     # {
     #     '$match': {
     #         '$expr': {
@@ -68,44 +86,6 @@ def extract_numbers_from_brackets(line):
         numbers = match.split(',')
         extracted_data.append(numbers)
     return extracted_data
-
-def txt_to_csv(input_file, output_file):
-    with open(input_file, 'r') as txt_file:
-        lines = txt_file.readlines()
-    
-    all_data = []
-    for line in lines:
-        extracted_data = extract_numbers_from_brackets(line)
-        all_data.extend(extracted_data)
-    
-    # Determine the maximum number of columns
-    max_columns = max(len(row) for row in all_data)
-
-    with open(output_file, 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        
-        for row in all_data:
-            # Extend row with empty strings if it has less columns than max_columns
-            row.extend([''] * (max_columns - len(row)))
-            csv_writer.writerow(row)
-
-
-def fix_txt(input,output):
-    print("in fix_txt")
-    # Read the data from the text file
-    with open(input, 'r') as file:
-        data = file.read()
-
-    # Convert the string representation of the list into an actual list
-    data_list = eval(data)
-
-    # Filter out sublists that have more than one number
-    filtered_list = [sublist for sublist in data_list if len(sublist) > 0 ]
-    filtered_list = [sublist for sublist in filtered_list if len(sublist) < 6 ]
-
-    # Optionally, save the filtered list back to a file
-    with open(output, 'w') as file1:
-        file1.write(str(filtered_list))
 
 # Fetch compound scores and store them in a dictionary for quick access
 cumulative_scores_total = list()
@@ -160,12 +140,3 @@ input_file_1 = 'chains_scores_v1.txt'  # Replace with your input text file name
 output_file_1 = 'fixed_scores_v1.txt'  # Replace with your input text file name
 with open(input_file_1, "w") as file:
     file.write(f"{cumulative_scores_total}\n")
-'''
-fix_txt(input_file_1,output_file_1)
-
-input_file_2 = output_file_1
-output_file_2 = 'output_file_2.csv'  # Replace with your desired output CSV file name
-txt_to_csv(input_file_2, output_file_2)
-print(f"Conversion complete. {output_file_2} has been created.")
-
-'''
